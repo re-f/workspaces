044790c14661069fbc9a82cdf399ac4273a75cfe
Ref <z.ref@outlook.com>
2021-11-03 11:46:58 +0000

    make private info configurable

--- Dockerfile.workspace/init.sh
+++ Dockerfile.workspace/init.sh
@@ -3,23 +3,34 @@
 CONFIG_PATH="${HOME}/.config"
 CONTAINER_ALREADY_STARTED="${CONFIG_PATH}/CONTAINER_ALREADY_STARTED_PLACEHOLDER"
 if [ ! -e "${CONTAINER_ALREADY_STARTED}" ]; then
-    touch "${CONTAINER_ALREADY_STARTED}"
     echo "-- First container startup --"
-    # config git
-    git config --global user.name "Ref"
-    git config --global user.email "z.reg@outlook.com"
-    
-    # gen ssh key
-    ssh_path=${CONFIG_PATH}/.ssh
-    mkdir "${ssh_path}"
+    echo "# config git"
+    git config --global user.name "${name}"
+    git config --global user.email "${personal_mail}"
 
-    chmod 700 "${ssh_path}"
-    ssh-keygen -t rsa -C "z.reg@outlook.com" -f "${ssh_path}/id_rsa" -q -N ""
+    echo "# check profile"
+    if [ -z "${name}" ]; then
+        echo "\${name} is empty, please fill in it in env/profile"
+    fi
 
-    work_mail=$(cat "${HOME}"/.config/profile/WORK_MAIL)
-    ssh-keygen -t rsa -C "${work_mail}" -f "${ssh_path}/id_rsa.company" -q -N ""
+    if [ ! -e "${ssh_path}" ]; then
+        echo "# gen ssh key"
+        ssh_path=${CONFIG_PATH}/ssh
 
+        mkdir "${ssh_path}"
+
+        chmod 700 "${ssh_path}"
+
+        ssh-keygen -t rsa -C "${personal_mail}" -f "${ssh_path}/id_rsa" -q -N ""
+        ssh-keygen -t rsa -C "${work_mail}" -f "${ssh_path}/id_rsa.company" -q -N ""
+
+    fi
+
+    echo "# download oh-my-zsh"
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-
+
+    echo "# download go devel tools"
+    go env -w GO111MODULE=on
     go get github.com/uudashr/gopkgs/v2/cmd/gopkgs
     go get github.com/ramya-rao-a/go-outline
     go get github.com/cweill/gotests/gotests
@@ -30,11 +41,11 @@ if [ ! -e "${CONTAINER_ALREADY_STARTED}" ]; then
     go get github.com/go-delve/delve/cmd/dlv@master
     go get honnef.co/go/tools/cmd/staticcheck
     go get golang.org/x/tools/gopls
+
+    echo "-- First container startup end--"
+    touch "${CONTAINER_ALREADY_STARTED}"
 fi
 
-
-while true;
-do
-    sleep 1;
+while true; do
+    sleep 1
 done
-

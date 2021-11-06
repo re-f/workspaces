9adc422d28a196af883f92512d7064fa246ddd52
Ref <z.ref@outlook.com>
2021-11-06 13:02:23 +0000

    fix

--- Dockerfile.workspace/init.sh
+++ Dockerfile.workspace/init.sh
@@ -5,17 +5,22 @@ CONTAINER_ALREADY_STARTED="${CONFIG_PATH}/CONTAINER_ALREADY_STARTED_PLACEHOLDER"
 if [ ! -e "${CONTAINER_ALREADY_STARTED}" ]; then
     echo "-- First container startup --"
     echo "# config git"
-    git config --global user.name "${name}"
-    git config --global user.email "${personal_mail}"
+
+    if [ ! -e "${CONFIG_PATH}/git/config" ]; then
+        git config --global user.name "${name}"
+        git config --global user.email "${personal_mail}"
+    else
+        echo "# ${CONFIG_PATH}/git/config exist ignored config git"
+    fi
 
     echo "# check profile"
     if [ -z "${name}" ]; then
         echo "\${name} is empty, please fill in it in env/profile"
     fi
 
+    ssh_path=${CONFIG_PATH}/ssh
     if [ ! -e "${ssh_path}" ]; then
         echo "# gen ssh key"
-        ssh_path=${CONFIG_PATH}/ssh
 
         mkdir "${ssh_path}"
 
@@ -23,7 +28,8 @@ if [ ! -e "${CONTAINER_ALREADY_STARTED}" ]; then
 
         ssh-keygen -t rsa -C "${personal_mail}" -f "${ssh_path}/id_rsa" -q -N ""
         ssh-keygen -t rsa -C "${work_mail}" -f "${ssh_path}/id_rsa.company" -q -N ""
-
+    else
+        echo "# ${ssh_path} exist ignored config ssh"
     fi
 
     echo "# download oh-my-zsh"
@@ -31,16 +37,15 @@ if [ ! -e "${CONTAINER_ALREADY_STARTED}" ]; then
 
     echo "# download go devel tools"
     go env -w GO111MODULE=on
-    go get github.com/uudashr/gopkgs/v2/cmd/gopkgs
-    go get github.com/ramya-rao-a/go-outline
-    go get github.com/cweill/gotests/gotests
-    go get github.com/fatih/gomodifytags
-    go get github.com/josharian/impl
-    go get github.com/haya14busa/goplay/cmd/goplay
-    go get github.com/go-delve/delve/cmd/dlv
-    go get github.com/go-delve/delve/cmd/dlv@master
-    go get honnef.co/go/tools/cmd/staticcheck
-    go get golang.org/x/tools/gopls
+    go get -x github.com/josharian/impl@latest
+    go get -x github.com/uudashr/gopkgs/v2/cmd/gopkgs@latest
+    go get -x github.com/ramya-rao-a/go-outline@latest
+    go get -x -d github.com/go-delve/delve/cmd/dlv@master
+    go get -x github.com/haya14busa/goplay/cmd/goplay@latest
+    go get -x honnef.co/go/tools/cmd/staticcheck@latest
+    go get -x github.com/go-delve/delve/cmd/dlv@latest
+    go get -x golang.org/x/tools/gopls@latest
+    go get -x github.com/rogpeppe/godef@latest
 
     echo "-- First container startup end--"
     touch "${CONTAINER_ALREADY_STARTED}"
